a41fedcfd658fc6bbc815be733f15f8188887405
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2019-05-22 16:46:48 -0500

    Update Codigo.bat

--- Codigo.bat
+++ Codigo.bat
@@ -8,12 +8,12 @@ set strA=%elemento:á=a%
 rename "%elemento%" "%strA%" 
 set strE=%strA:é=e%
 rename "%strA% "%strE%"
-set strI=%strE:f=i%
+set strI=%strE:í=i%
 rename "%strE%" "%strI%"
 set strO=%strI:ó=o%
 rename "%strI%" "%strO%"
 set strU=%strO:ú=u%
 rename "%strO%" "%strU%"
-set strN=%strU:ņ=n%
+set strN=%strU:ñ=n%
 rename "%strU%" "%strN%"
 GOTO :EOF
