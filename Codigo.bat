051e184575f2af61ad315126070bbe44e13322e8
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2019-05-22 13:02:12 -0500

    Add files via upload

---
+++ Codigo.bat
@@ -0,0 +1,19 @@
+Chcp 65001
+@ECHO off
+setlocal Prueba
+FOR %%f IN (*) DO ( set elemento=%%f&call :Cambiar )
+GOTO :EOF
+:Cambiar
+set strA=%elemento:á=a%
+rename "%elemento%" "%strA%" 
+set strE=%strA:é=e%
+rename "%strA% "%strE%"
+set strI=%strE:f=i%
+rename "%strE%" "%strI%"
+set strO=%strI:ó=o%
+rename "%strI%" "%strO%"
+set strU=%strO:ú=u%
+rename "%strO%" "%strU%"
+set strN=%strU:ņ=n%
+rename "%strU%" "%strN%"
+GOTO :EOF
