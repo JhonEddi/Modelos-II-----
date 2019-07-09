fd735f741aa6b691b7d24e07eaafc05d0bdcca9e
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2019-07-09 10:13:32 -0500

    Actualizacion del codigo

--- Codigo.bat
+++ Codigo.bat
@@ -1,19 +1,31 @@
-Chcp 10075
+chcp 65001
 @ECHO off
-setlocal Prueba
-FOR %%f IN (*) DO ( set elemento=%%f&call :Cambiar )
+FOR %%f IN (*) DO (
+    set archivo=%%f&call :renombrar
+)
 GOTO :EOF
-:Cambiar
-set strA=%elemento:á=a%
-rename "%elemento%" "%strA%" 
-set strE=%strA:é=e%
-rename "%strA% "%strE%"
-set strI=%strE:í=i%
-rename "%strE%" "%strI%"
-set strO=%strI:ó=o%
-rename "%strI%" "%strO%"
-set strU=%strO:ú=u%
-rename "%strO%" "%strU%"
-set strN=%strU:ñ=n%
-rename "%strU%" "%strN%"
+set str1=%archivo:á=a%
+rename "%archivo%" "%str1%"
+set str2=%str1:é=e%
+rename "%str1%" "%str2%"
+set str3=%str2:í=i%
+rename "%str2%" "%str3%"
+set str4=%str3:ó=o%
+rename "%str3%" "%str4%"
+set str5=%str4:ú=u%
+rename "%str4%" "%str5%"
+set str6=%str5:ñ=n%
+rename "%str5%" "%str6%"
+set str7=%str6:Á=A%
+rename "%str6%" "%str7%"
+set str8=%str7:É=E%
+rename "%str7%" "%str8%"
+set str9=%str8:Í=I%
+rename "%str8%" "%str9%"
+set str10=%str9:Ó=O%
+rename "%str9%" "%str10%"
+set str11=%str10:Ú=U%
+rename "%str10%" "%str11%"
+set str12=%str11:Ñ=N%
+rename "%str11%" "%str12%"
 GOTO :EOF
